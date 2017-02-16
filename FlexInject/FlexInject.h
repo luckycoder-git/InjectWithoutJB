638c7a4e73a8cada200f6ae8d0140ab41f8fe404
罗红瑞 <luohongrui@123juzi.com>
2017-02-16 14:28:53 +0800

    Initial Commit

---
+++ FlexInject/FlexInject.h
@@ -0,0 +1,13 @@
+//
+//  FlexInject.h
+//  FlexInject
+//
+//  Created by 罗红瑞 on 2017/2/16.
+//  Copyright (c) 2017年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlexInject : NSObject
+
+@end
---
+++ FlexInject/FlexInject.m
@@ -0,0 +1,22 @@
+//
+//  FlexInject.m
+//  FlexInject
+//
+//  Created by 罗红瑞 on 2017/2/16.
+//  Copyright (c) 2017年 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlexInject.h"
+
+@implementation FlexInject
+
+-(id)init
+{
+	if ((self = [super init]))
+	{
+	}
+    
+	return self;
+}
+
+@end
